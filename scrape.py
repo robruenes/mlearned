aa8cf808264bd765581fb95d32d67db6aa7a9a31
Rob Ruenes <rob@ruenes.dev>
2023-11-25 12:17:33 -0500

    Add some docstrings, do some cleanups, add progress via print statements, actually store data scraped in data object, add (commented out) navigation to individual match pages.

--- scrape.py
+++ scrape.py
@@ -6,55 +6,6 @@ from io import StringIO
 from playwright.sync_api import sync_playwright
 
 
-def log_in(page):
-    page.goto("https://www.learnedleague.com/")
-    page.fill('#sidebar input[name="username"]', os.environ["LL_USER"])
-    page.fill('#sidebar input[name="password"]', os.environ["LL_PASS"])
-    page.click('#sidebar input[type="submit"]')
-
-
-def get_urls(friend_id):
-    base_url = "https://www.learnedleague.com/profiles.php?{id}".format(id=friend_id)
-    urls = {
-        "latest": base_url + "&1",
-        "stats": base_url + "&2",
-        "past seasons": base_url + "&7",
-    }
-    return urls
-
-
-def scrape_latest_data(friend_id, data, page, url):
-    page.goto(url)
-    table = page.locator("div.fl_latest.fl_l_l.pldata").inner_html()
-    df = pd.read_html(StringIO(table))[0]
-    categorical = df[df["Category"].map(lambda category: category != "TOTALS")]
-    categorical["Category"].replace(
-        {
-            "AMER HIST": 0,
-            "ART": 1,
-            "BUS/ECON": 2,
-            "CLASS MUSIC": 3,
-            "CURR EVENTS": 4,
-            "FILM": 5,
-            "FOOD/DRINK": 6,
-            "GAMES/SPORT": 7,
-            "GEOGRAPHY": 8,
-            "LANGUAGE": 9,
-            "LIFESTYLE": 10,
-            "LITERATURE": 11,
-            "MATH": 12,
-            "POP MUSIC": 13,
-            "SCIENCE": 14,
-            "TELEVISION": 15,
-            "THEATRE": 16,
-            "WORLD HIST": 17,
-        },
-        inplace=True,
-    )
-    # TODO: Decide how this will be stored, and doing something
-    # with the TOTALS row might be useful.
-
-
 def transform_rundle_value(rundle):
     rundle_mapping = {
         "R": 1,
@@ -67,13 +18,113 @@ def transform_rundle_value(rundle):
     return rundle_mapping[rundle[0]]
 
 
-def scrape_stats_data(friend_id, data, page, url):
+def transform_category_value(category):
+    category_mapping = {
+        "AMER HIST": 0,
+        "ART": 1,
+        "BUS/ECON": 2,
+        "CLASS MUSIC": 3,
+        "CURR EVENTS": 4,
+        "FILM": 5,
+        "FOOD/DRINK": 6,
+        "GAMES/SPORT": 7,
+        "GEOGRAPHY": 8,
+        "LANGUAGE": 9,
+        "LIFESTYLE": 10,
+        "LITERATURE": 11,
+        "MATH": 12,
+        "POP MUSIC": 13,
+        "SCIENCE": 14,
+        "TELEVISION": 15,
+        "THEATRE": 16,
+        "WORLD HIST": 17,
+    }
+    return category_mapping[category]
+
+
+def log_in(page):
+    page.goto("https://www.learnedleague.com/")
+    page.fill('#sidebar input[name="username"]', os.environ["LL_USER"])
+    page.fill('#sidebar input[name="password"]', os.environ["LL_PASS"])
+    page.click('#sidebar input[type="submit"]')
+
+
+def get_urls_for_friend(friend_id):
+    base_url = "https://www.learnedleague.com/profiles.php?{id}".format(id=friend_id)
+    urls = {
+        "latest": base_url + "&1",
+        "stats": base_url + "&2",
+        "past seasons": base_url + "&7",
+    }
+    return urls
+
+
+def scrape_latest_data(page, url):
+    """
+    Returns a dataframe with the following columns:
+
+    - Category: The category of the question, using the numerical representation
+        from the method `transform_category_value` above.
+    - Percent Correct: The percentage of questions of this category the player has answered correctly.
+    - Num Correct: Total number of questions in this category answered correctly.
+    - Num Incorrect: Total number of questions in this category answered incorrectly.
+    """
+
+    page.goto(url)
+    table = page.locator("div.fl_latest.fl_l_l.pldata").inner_html()
+
+    # We expect that there's only a single table on the page.
+    df = pd.read_html(StringIO(table))[0]
+
+    # Ignore columns specific to the current season, just look
+    # at historical stats.
+    df = df.filter(["Category", "Career", "%"])
+    df.rename(columns={"%": "Percent Correct"}, inplace=True)
+
+    # Drop totals, and sanitize categorical stats.
+    df.drop(df[df["Category"] == "TOTALS"].index, inplace=True)
+    df["Category"] = df["Category"].transform(transform_category_value)
+    df.sort_values(by=["Category"], inplace=True)
+    df[["Num Correct", "Num Incorrect"]] = df["Career"].str.split("-", expand=True)
+    df.drop(["Career"], axis=1, inplace=True)
+    df.reset_index(inplace=True, drop=True)
+
+    return df
+
+
+def scrape_stats_data(page, url):
+    """
+    Returns a dataframe with the following columns:
+
+    - Wins: Total number of wins across all player history.
+    - Losses: Total number of losses across all player history.
+    - Ties: Total number of ties across all player history.
+    - Points in Standings: Determines where the player is in overall
+        standings. 2 points for win, 1 for tie, -1 for forfeit.
+    - Match Points Differential: Difference between points scored and
+        points allowed.
+    - Total Match Points: Sum of points scored in all matches.
+    - Total Correct Answers: Sum of questions answered correctly
+        in all matches.
+    - Total Points Allowed: Sum of points scored by opponents in all matches.
+    - Correct Answers Allowed: Total number of questions answered correctly
+        by opponents in all matches.
+    - Unforced Points Allowed: Total number of points allowed above that
+        which would have been allowed with perfect defense.
+    - Defensive Efficiency: Measure of how good the player is defensively.
+        The higher the better.
+    - Wins by Forfeit: Self explanatory.
+    - Losses by Forfeit: Self explanatory.
+    - 3 point questions answered correctly: Self explanatory.
+    """
+
     page.goto(url)
     table = page.locator(".statscontainer").inner_html()
     df = pd.read_html(StringIO(table))[0]
 
-    # Remove the rank column.
-    df.drop(["Rank"], axis=1, inplace=True)
+    # Remove columns we don't need (Rank), or which don't have a clear
+    # explanation found on the site (PCAA, MCW, QPct)
+    df.drop(["Rank", "PCAA", "MCW", "QPct"], axis=1, inplace=True)
 
     # Drop career and per-rundle aggregated statistics.
     df = df[df["Season"].map(lambda season: season.startswith("LL"))]
@@ -81,23 +132,64 @@ def scrape_stats_data(friend_id, data, page, url):
     # Transform the rundle string into a numerical value.
     df["Rundle"] = df["Rundle"].transform(transform_rundle_value)
 
+    df.rename(
+        columns={
+            "W": "Wins",
+            "L": "Losses",
+            "T": "Ties",
+            "PTS": "Points in Standings",
+            "MPD": "Match Points Differential",
+            "TMP": "Total Match Points",
+            "TCA": "Total Correct Answers",
+            "TPA": "Total Points Allowed",
+            "CAA": "Correct Answers Allowed",
+            "UfPA": "Unforced Points Allowed",
+            "DE": "Defensive Efficiency",
+            "FW": "Wins by Forfeit",
+            "FL": "Losses by Forfeit",
+            "3PT": "3 point questions answered correctly",
+        },
+        inplace=True,
+    )
+
     # Drop the now unnecessary season column, and reindex
     # to account for dropped columns.
     df.drop(["Season"], axis=1, inplace=True)
-    df.reset_index(inplace=True)
-    # TODO: Decide how we'll ultimately store this,
-    # and limit to the same number of rows per player.
+    df.reset_index(inplace=True, drop=True)
+
+    return df
 
 
-def scrape_match_day_history(friend_id, data, page, url):
+def scrape_match_day_history(page, url, browser):
+    """
+    Returns a mapping of season names (e.g. "LL99") to dataframes
+    representing the results for every match in that season, with
+    the following columns:
+
+    - Result: Representation of whether the player won (3), tied (2),
+        lost (1), or forfeited (0).
+    - Points: Total number of points the player scored.
+    - Correct: Total number of questions answered correctly.
+    - Opponent Points: Total number of points the opponent scored.
+    - Opponent Correct: Total number of questions the opponend
+        answered correctly.
+
+    In the future this method will return more detailed statistics
+    per match.
+    """
     page.goto(url)
+
+    # Mapping of seasons to dataframes with stats for that season.
+    season_to_matches = {}
+
     # Get all tables except the first, which isn't match data.
     tables = page.locator("div.fl_latest.fl_l_l").all()[1:]
     for t in tables:
-        # TODO: Use this HTML to navigate to the individual
-        # match days.
-        t_html = t.inner_html()
-        df = pd.read_html(StringIO(t_html))[0]
+        # This is of the form "LL#", where # is the season (e.g. "LL99")
+        season = t.get_by_role("link").nth(0).inner_html()
+
+        # Collect high level match day data for the season
+        df = pd.read_html(StringIO(t.inner_html()))[0]
         df.drop(["Match Day", "Opponent", "Record", "Rdl Rk"], axis=1, inplace=True)
         df["Result"].replace({"W": 3, "T": 2, "L": 1, "F": 0}, inplace=True)
         df["Result.1"] = df["Result.1"].str.replace(r"\(|\)-", " ", regex=True)
@@ -109,15 +201,41 @@ def scrape_match_day_history(friend_id, data, page, url):
         df["Opponent Correct"].replace({"F": 0}, inplace=True)
         df.drop("Result.1", axis=1, inplace=True)
 
+        season_to_matches[season] = df
 
-def scrape_friend_data(friend_id, data, page):
-    for page_type, url in get_urls(friend_id).items():
+        # Collect links of every match
+        # links = t.get_by_role("link").filter(has_text=")-").all()
+        # match_pages = [
+        #     "https://www.learnedleague.com{match_selector}".format(
+        #         match_selector=l.get_attribute("href")
+        #     )
+        #     for l in links
+        # ]
+
+        # # We need to create a new page here, otherwise the outer
+        # # loop gets stuck.
+        # new_page = browser.new_page()
+        # log_in(new_page)
+        # for match in match_pages:
+        #     new_page.goto(match)
+
+    return season_to_matches
+
+
+def scrape_friend_data(friend_id, data, page, browser):
+    friend_name = data["name"]
+    for page_type, url in get_urls_for_friend(friend_id).items():
         if page_type == "latest":
-            scrape_latest_data(friend_id, data, page, url)
+            print("Scraping latest data for {friend}...".format(friend=friend_name))
+            data["latest"] = scrape_latest_data(page, url)
+
         elif page_type == "stats":
-            scrape_stats_data(friend_id, data, page, url)
-        if page_type == "past seasons":
-            scrape_match_day_history(friend_id, data, page, url)
+            print("Scraping stats data for {friend}...".format(friend=friend_name))
+            data["stats"] = scrape_stats_data(page, url)
+
+        elif page_type == "past seasons":
+            print("Scraping match data for {friend}...".format(friend=friend_name))
+            data["season_to_matches"] = scrape_match_day_history(page, url, browser)
 
 
 def scrape_data(friends):
@@ -126,7 +244,7 @@ def scrape_data(friends):
         page = browser.new_page()
         log_in(page)
         for friend_id, data in friends.items():
-            scrape_friend_data(friend_id, data, page)
+            scrape_friend_data(friend_id, data, page, browser)
 
 
 if __name__ == "__main__":

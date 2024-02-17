4581b32bc5a3c664c827d91affae45db284007a8
Rob Ruenes <rob@ruenes.dev>
2024-02-17 16:37:12 -0500

    Add categorical scoring stats into the training set.

--- generate_training_set.py
+++ generate_training_set.py
@@ -41,8 +41,51 @@ def write_per_player_aggregated_csvs(player_to_files, data_path, agg_path):
         career_stats = pd.read_csv(
             f"{player_data_path}/career_stats.csv", sep="\t", encoding="utf-8"
         )
+
+        # TODO: There's gotta be a better way to do this
         for col in career_stats.columns:
             df[col] = career_stats[col].values[0]
+
+        # Broadcast categorical performance.
+        categorical_performance = pd.read_csv(
+            f"{player_data_path}/latest_league_stats.csv", sep="\t", encoding="utf-8"
+        )
+        categorical_performance = categorical_performance.filter(
+            ["Category", "Percent Correct"]
+        )
+
+        categorical_performance["Category"] = categorical_performance[
+            "Category"
+        ].replace(
+            {
+                0: "AMER HIST Pct Correct",
+                1: "ART Pct Correct",
+                2: "BUS/ECON Pct Correct",
+                3: "CLASS MUSIC Pct Correct",
+                4: "CURR EVENTS Pct Correct",
+                5: "FILM Pct Correct",
+                6: "FOOD/DRINK Pct Correct",
+                7: "GAMES/SPORT Pct Correct",
+                8: "GEOGRAPHY Pct Correct",
+                9: "LANGUAGE Pct Correct",
+                10: "LIFESTYLE Pct Correct",
+                11: "LITERATURE Pct Correct",
+                12: "MATH Pct Correct",
+                13: "POP MUSIC Pct Correct",
+                14: "SCIENCE Pct Correct",
+                15: "TELEVISION Pct Correct",
+                16: "THEATRE Pct Correct",
+                17: "WORLD HIST Pct Correct",
+            }
+        )
+
+        categorical_performance = categorical_performance.T.reset_index(drop=True)
+        categorical_performance.columns = categorical_performance.iloc[0]
+        categorical_performance = categorical_performance.iloc[1:, :]
+
+        # TODO: There's gotta be a better way to do this
+        for col in categorical_performance.columns:
+            df[col] = categorical_performance[col].values[0]
 
         if not os.path.exists(agg_path):
             os.makedirs(agg_path)
